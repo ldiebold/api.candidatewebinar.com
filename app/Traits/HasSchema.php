5c6158560af79933a5314d1db8d56a08528c1266
ldiebold <luke@ldiebold.com>
2020-11-27 15:10:33 +1100

    WIP

--- app/Traits/HasSchema.php
+++ app/Traits/HasSchema.php
@@ -6,7 +6,7 @@ trait HasSchema
 {
     static function getSchema()
     {
-        $file_root = base_path('../orm-classes/src/schemas');
+        $file_root = base_path('./orm-classes/src/schemas');
         $file_path = $file_root . '/' . substr(strrchr(__CLASS__, "\\"), 1) . '.json';
 
         $jsonSchemaString = file_get_contents($file_path);
